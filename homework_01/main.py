225c0e27daa0abb2268a1db6cdaba8e41a156db0
Max Vetrov <maksim.vetrov@webpp.ru>
2022-12-14 19:52:02 +0300

    Реализована функция фильтрации элементов списка

--- homework_01/main.py
+++ homework_01/main.py
@@ -24,18 +24,49 @@ ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+NEXT_NUMBER = 1
+DEEGRE_ORDER = 2
+BASE_DIVIDER = 2
+ZERO = 0
 
-def filter_numbers():
+
+def is_prime(number) -> bool:
+    """ Функция определяет, является ли число простым. """
+    for item in range(BASE_DIVIDER, (number//DEEGRE_ORDER) + NEXT_NUMBER):
+        if number % item == ZERO:
+            return False
+    return True
+
+
+def is_even(number) -> bool:
+    """ Функция определяет, является ли число чётным. """
+    result = number % BASE_DIVIDER
+    return result == ZERO
+
+
+def is_odd(number) -> bool:
+    """ Функция определяет, является ли число нечётным. """
+    result = number % BASE_DIVIDER
+    return result != ZERO
+
+
+def filter_numbers(numbers, filter_type) -> list:
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
     (выбор производится передачей дополнительного аргумента)
-
-    #>>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
-    #>>> filter_numbers([2, 3, 4, 5], EVEN)
-    <<< [2, 4]
     """
+    if filter_type == EVEN:
+        result = filter(is_even, numbers)
+
+    if filter_type == ODD:
+        result = filter(is_odd, numbers)
+
+    if filter_type == PRIME:
+        result = filter(is_prime, numbers)
+
+    return list(result)
 
 
-print(power_numbers(1, 2, 5, 7, 9))
+filtered = filter_numbers([1, 2, 3, 6, 9], PRIME)
+print(filtered)

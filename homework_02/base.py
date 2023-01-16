5385f258fcc511edfcee691abcd6ef8c51cf0471
Max Vetrov <maksim.vetrov@webpp.ru>
2023-01-16 17:39:00 +0300

    Объявлены исключения, доработан класс Vehicle

--- homework_02/base.py
+++ homework_02/base.py
@@ -2,4 +2,11 @@ from abc import ABC
 
 
 class Vehicle(ABC):
-    pass
+    """ Класс, описывающий транспортное средство. """
+    started = False
+
+    def __init__(self, weight: float, fuel: float, fuel_consumption: float):
+        """ Конструктор. """
+        self.weight = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption

492dcc1da589287d77c1cd54d8a5813c4881e30a
Max Vetrov <maksim.vetrov@webpp.ru>
2023-01-17 12:26:44 +0300

    Доработки класса Plane

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -6,7 +6,7 @@ from .exceptions import CargoOverload
 """
 
 class Plane(Vehicle):
-    cargo: float
+    cargo: float = 10.5
     max_cargo: float
 
     def __init__(self, max_cargo: float):
@@ -14,7 +14,7 @@ class Plane(Vehicle):
 
     def load_cargo(self, add_cargo: float):
         total_cargo = self.cargo + add_cargo
-        
+
         if total_cargo <= self.max_cargo:
             self.cargo = total_cargo
         else:

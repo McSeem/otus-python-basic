47b207d7172e586624c2bc2f90f5728db798d635
Max Vetrov <maksim.vetrov@webpp.ru>
2023-02-26 11:45:39 +0300

    Исправлен путь для view

--- homework_03/views/pingpong/views.py
+++ homework_03/views/pingpong/views.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, status, HTTPException
 router = APIRouter(prefix="/ping", tags=["Ping"])
 
 
-@router.get("", status_code=status.HTTP_200_OK)
+@router.get("/", status_code=status.HTTP_200_OK)
 def get_ping():
     return {
         "message": "pong",

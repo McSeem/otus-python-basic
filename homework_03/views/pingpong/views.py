321bf0ec8316c95d6a70b1b2ed36b9bd1dadab89
Max Vetrov <maksim.vetrov@webpp.ru>
2023-02-27 16:41:26 +0300

    Добавлена выдача исключения при обработке запроса

--- homework_03/views/pingpong/views.py
+++ homework_03/views/pingpong/views.py
@@ -4,7 +4,10 @@ router = APIRouter(prefix="/ping", tags=["Ping"])
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
-def get_ping():
+@router.get("/{wrong_ping}", status_code=status.HTTP_200_OK)
+def get_ping(wrong_ping: str = '') -> dict:
+    if wrong_ping != '':
+        raise HTTPException(status_code=404, detail="Ping from your client is bad")
     return {
         "message": "pong",
     }

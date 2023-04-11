cd2b4cc0252d09f69653591ac8f190c876561e57
Max Vetrov <maksim.vetrov@webpp.ru>
2023-04-11 15:21:13 +0300

    HWRK-05 Добавлен Dockerfile, а также одельный модуль для запуска приложения

--- homework_05/app.py
+++ homework_05/app.py
@@ -10,3 +10,13 @@
 в базовый шаблон добавьте навигационную панель nav (https://getbootstrap.com/docs/5.0/components/navbar/)
 в навигационную панель добавьте ссылки на главную страницу / и на страницу /about/ при помощи url_for
 """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello, World"

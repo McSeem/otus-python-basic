cddf211e152c0a3d08e7b8717bb24ad6fc57a2dd
Max Vetrov <maksim.vetrov@webpp.ru>
2023-04-27 22:38:49 +0300

    HWRK-04 Отладка getcwd, изменён абсолютный путь к файлу БД

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 SQLALCHEMY_PG_CONN_URI = "postgresql+pg8000://postgres:testpass@localhost:5432/blog"
-SQLALCHEMY_SQLITE_CONN_URI = "sqlite:////home/runner/work/otus-python-basic/db/blog.db"
+#SQLALCHEMY_SQLITE_CONN_URI = "sqlite:////home/runner/work/otus-python-basic/db/blog.db"
+SQLALCHEMY_SQLITE_CONN_URI = "sqlite:////home/runner/work/otus-python-basic/otus-python-basic/db/blog.db"
 DB_TYPE = 0
 DB_ECHO = False

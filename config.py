b2614ca2b881694de2eaafa4f2499a7e327cbfeb
Max Vetrov <maksim.vetrov@webpp.ru>
2023-05-02 14:23:11 +0300

    HWRK-05 Доработан основной config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_PG_CONN_URI = "postgresql+pg8000://postgres:testpass@localhost:5432/blog"
+SQLALCHEMY_PG_CONN_URI = "postgresql+asyncpg://postgres:testpass@localhost:5432/blog"
 SQLALCHEMY_SQLITE_CONN_URI = "sqlite+aiosqlite:///../db/blog.db"
 DB_TYPE = 1
 DB_ECHO = False

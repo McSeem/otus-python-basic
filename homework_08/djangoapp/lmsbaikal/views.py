4958fc46eb9ed4537307919de9f22fe6b755ec20
Max Vetrov <maksim.vetrov@webpp.ru>
2023-05-29 15:34:57 +0300

    Добавлено представление для списка вопросов

--- homework_08/djangoapp/lmsbaikal/views.py
+++ homework_08/djangoapp/lmsbaikal/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("LMS Baikal")
+    question_list = Question.objects.order_by("id")[:5]
+    output = ", ".join([q.question_text for q in question_list])
+
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)

5ecddd8f7b2de4e6d8552560ea3aa76ed79908e7
Max Vetrov <maksim.vetrov@webpp.ru>
2023-05-30 17:50:40 +0300

    Добавлены тест представления для страницы списка вопросов

--- homework_08/djangoapp/lmsbaikal/tests.py
+++ homework_08/djangoapp/lmsbaikal/tests.py
@@ -4,24 +4,21 @@ from .models import Question
 
 
 class TestQuestion(TestCase):
-    def test_init(self):
-        question = Question.objects.create(name="Question 1")
+    def setUp(self):
+        self.question = Question.objects.create(name="Question 1")
 
-        self.assertEqual(question.name, "Question 1")
+    def test_init(self):
+        self.assertEqual(self.question.name, "Question 1")
 
     def test_str(self):
-        question = Question.objects.create(name="Question 1")
-
-        self.assertEqual(str(question), "Question 1")
+        self.assertEqual(str(self.question), "Question 1")
 
     def test_question_tags_count(self):
-        question = Question.objects.create(name="Question 1")
-
         tags = ["tag1", "tag2", "tag3"]
 
         for tag in tags:
-            question.tags.append(tag)
+            self.question.tags.append(tag)
 
-        question.save()
+        self.question.save()
 
-        self.assertEqual(question.get_tags_count(), 3)
+        self.assertEqual(self.question.get_tags_count(), 3)
